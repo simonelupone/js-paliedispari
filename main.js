ba87e4368bbf7507a0cc7b42dd98c2469a4c5a6b
simonelupone <simone.lupone@gmail.com>
2025-03-21 15:57:24 +0100

    [feat]: add function to check if a word is palindrome

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 // Palindroma
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
+function isPalindrome(word) {
+    // controllo che nella parola non ci siano spazi ed eventualmente li tolgo
+    word = word.toLowerCase().replace(/\s/g, '');
+    // confronto la parola originale con l'inverso e torna true se coincidono
+    return word === word.split('').reverse().join('');
+}
+
 // Pari e Dispari
 // L’utente sceglie pari o dispari e
 // inserisce un numero da 1 a 5. Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).
